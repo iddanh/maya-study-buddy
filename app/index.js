636f4576c3a2c0dd5c0048d34d7fb2d4ba9cfc70
Iddan <iddanh@gmail.com>
2018-07-11 22:57:16 +0300

    added routing

--- app/index.js
+++ app/index.js
@@ -1,11 +1,39 @@
 (function () {
 	'use strict';
 
-	const app = angular.module('app', ['cloudinary']);
+	const app = angular.module('app', ['cloudinary', 'ui.router']);
 
-	app.config(function (cloudinaryProvider) {
+	app.config(function (cloudinaryProvider, $locationProvider, $stateProvider) {
 		cloudinaryProvider
 			.set("cloud_name", "hdzc7seee");
+
+		$locationProvider.html5Mode(true);
+
+		$stateProvider.state('default', {
+			url: '/{roomName}',
+			templateUrl: '/templates/default.html',
+			controller: 'mainController',
+			controllerAs: 'vm',
+			resolve: {
+				room: function ($transition$) {
+					const database = firebase.database();
+					return database.ref('/rooms/').once('value').then((snapshot) => {
+						const rooms = snapshot.val();
+						const roomName = $transition$.params().roomName;
+						if (!roomName) {
+							return rooms[0];
+						}
+						return rooms.filter(room => room.name === roomName)[0] || rooms[0];
+
+					});
+				}
+			}
+		}).state('admin', {
+			url: '/mayacool',
+			templateUrl: '/templates/admin.html',
+			controller: 'adminController',
+			controllerAs: 'vm'
+		})
 	});
 
 	app.filter('underscoreless', function () {
@@ -17,11 +45,37 @@
 		}
 	});
 
-	app.controller('mainController', function ($http, $filter) {
+	app.controller('rootController', function ($location) {
+		const vm = this;
+		vm.$onInit = () => {
+			firebase.database().ref('/rooms/').once('value').then((snapshot) => {
+				vm.rooms = snapshot.val();
+				vm.selectedRoom = vm.rooms[0].name;
+			});
+
+			vm.selected = selected;
+		};
+
+		function selected() {
+			$location.url(`/${vm.selectedRoom}`);
+		}
+	});
+
+	app.controller('adminController', function () {
+		const vm = this;
+
+		vm.$onInit = () => {
+			vm.hello = 'world';
+		};
+	});
+
+	app.controller('mainController', function ($http, $filter, $location, room) {
 
 		const vm = this;
 
 		vm.$onInit = () => {
+			console.log(room);
+
 			vm.uploadImages = uploadImages;
 			vm.imageClick = imageClick;
 			vm.nextQuestion = nextQuestion;
@@ -31,7 +85,10 @@
 			vm.gameOver = false;
 			vm.index = 0;
 
-			getImages();
+			// getImages();
+
+			vm.room = room;
+			nextImage();
 		};
 
 		function uploadImages() {
@@ -68,16 +125,15 @@
 
 		function nextImage(removeLast) {
 			if (removeLast) {
-				vm.images.splice(vm.index, 1);
+				vm.room.images.splice(vm.index, 1);
 			}
 
-			if (vm.images.length === 0) {
+			if (vm.room.images.length === 0) {
 				return true;
 			}
 
-			vm.index = Math.floor(Math.random() * vm.images.length);
-			vm.image = vm.images[vm.index];
-			vm.overlayText = $filter('underscoreless')(vm.image.public_id);
+			vm.index = Math.floor(Math.random() * vm.room.images.length);
+			vm.image = vm.room.images[vm.index];
 		}
 
 		function getImages() {
